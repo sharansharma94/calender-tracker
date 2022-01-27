a0dd8cf513c2446b06e8f8fdc55d67b11c902799
sharansharma94 <sharansharma19@gmail.com>
2022-01-27 16:07:57 +0530

    react hok form added

--- frontend/src/Components/NewItem.tsx
+++ frontend/src/Components/NewItem.tsx
@@ -1,74 +1,11 @@
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-} from "@chakra-ui/react";
 import { Card } from "../StyledComponents/Card";
-import {
-  Description,
-  ItemFooter,
-  ItemWrapper,
-  SubTitle,
-  Title,
-} from "../StyledComponents/Item";
-import { Nav } from "./Nav";
 
-import styled from "styled-components";
-import {
-  Container,
-  ContainerColum,
-  SpaceBetweenItem,
-} from "../StyledComponents/Container";
+import NewItemForm from "./NewItemForm";
 
 export default function NewItem() {
   return (
     <Card>
-      <ItemWrapper>
-        <Nav>
-          <Title>
-            <FormControl>
-              <FormLabel htmlFor="title">Title</FormLabel>
-              <Input id="title" type="text" />
-            </FormControl>
-          </Title>
-          <Button colorScheme="var(--chakra-colors-gray-800)">Save</Button>
-        </Nav>
-        <Description>
-          <Textarea
-            // value={value}
-            // onChange={handleInputChange}
-            placeholder="Add Description of the task"
-            size="sm"
-          />
-        </Description>
-
-        <ItemFooter>
-          <ContainerColum>
-            <SubTitle>Duration</SubTitle>
-            <SpaceBetweenItem>
-              <div>
-                <FormControl>
-                  <FormLabel htmlFor="estimated">Estimated </FormLabel>
-                  <Input id="estimated" type="estimated" />
-                </FormControl>
-              </div>
-              <div>
-                <FormControl>
-                  <FormLabel htmlFor="actual">Actual</FormLabel>
-                  <Input id="actual" type="actual" />
-                </FormControl>
-              </div>
-            </SpaceBetweenItem>
-            <SpaceBetweenItem></SpaceBetweenItem>
-          </ContainerColum>
-        </ItemFooter>
-      </ItemWrapper>
+      <NewItemForm />
     </Card>
   );
 }
-
-export const DurationContainer = styled.div`
-  display: flex;
-`;
